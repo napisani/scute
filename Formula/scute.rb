0def6156e24d047ee8a6e516924f0fd7199898cd
Nick Pisani <napisani@yahoo.com>
2026-04-30 07:39:46 -0400

    Update version to 0.0.19

--- Formula/scute.rb
+++ Formula/scute.rb
@@ -1,18 +1,18 @@
 class Scute < Formula
   desc "AI-powered shell assistant"
   homepage "https://github.com/napisani/scute"
-  version "0.0.18"
+  version "0.0.19"
   license "MIT"
   license "MIT"
 
   on_macos do
     url "https://github.com/napisani/scute/releases/download/v#{version}/scute-v#{version}-macos-arm64.tar.gz"
-    sha256 "7bb30a33ebd0f16dbaf382d8797a8f253848a9491842516f249d3a8a14c51aa8"
+    sha256 "df70d62cf1fade274c21876988d76ed0adbec63afd5d99aeb4ff8e70c4cefe9e"
   end
 
   on_linux do
     url "https://github.com/napisani/scute/releases/download/v#{version}/scute-v#{version}-linux-x86_64.tar.gz"
-    sha256 "f4b089775caa6c90a3e61ae8140f6dfc4c90c5fc92ac841009bb1cd60d107ed1"
+    sha256 "0b50c1ce51a8c1bf8345c6a18a9279a84fab4a1ef1706d6193aaac70dcff5d43"
   end
 
   def install
